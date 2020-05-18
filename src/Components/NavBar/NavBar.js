14a716b00f7ebe83d542e829385bc65b67a7fda9
Mitch Perez <perez15@live.missouristate.edu>
2020-05-18 17:49:19 -0500

    initial file/page structure

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import '../Components.css';
+
+const NavBar = () =>{
+	return <nav className = 'backgr'>
+		Hello world
+	</nav>
+}
+
+export default NavBar;
